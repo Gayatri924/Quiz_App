817ed4820bb9fc3253dd754911435ae3d5f86e10
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-02-03 17:35:52 -0500

    mid step 5

--- app/src/main/java/com/gopavajhalagayatri/quizapp/MainActivity.java
+++ app/src/main/java/com/gopavajhalagayatri/quizapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.gopavajhalagayatri.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.util.Log;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton=findViewById(R.id.clickButton);
+        responseText=findViewById(R.id.responseEditText);
+        displayText=findViewById(R.id.textBox);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("testButton","it worked?!?!?!? "+responseText.getText().toString());
+            }
+        });
     }
 }
